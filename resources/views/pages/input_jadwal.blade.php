12abb966120ce8dabad4da9fd42c808e5b907a06
afdha12 <azri.afdha@icloud.com>
2024-07-22 09:26:27 +0700

    Add search based on date and pagination

--- resources/views/pages/input_jadwal.blade.php
+++ resources/views/pages/input_jadwal.blade.php
@@ -13,50 +13,50 @@
                         <div class="row mb-3">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Tanggal Operasi</label>
                             <div class="col-sm-8">
-                                <input type="date" class="form-control text-uppercase" id="tgl_operasi" name="tgl_operasi">
+                                <input type="date" class="form-control required text-uppercase" id="tgl_operasi" name="tgl_operasi">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Jam Operasi</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control text-uppercase" id="jam_operasi" name="jam_operasi">
+                                <input type="text" class="form-control required text-uppercase" id="jam_operasi" name="jam_operasi">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Nama Pasien</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control text-uppercase" id="nama_pasien" name="nama_pasien">
+                                <input type="text" class="form-control required text-uppercase" id="nama_pasien" name="nama_pasien">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Usia</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control text-uppercase" id="usia" name="usia">
+                                <input type="number" class="form-control required text-uppercase" id="usia" name="usia">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">No. CM</label>
                             <div class="col-sm-8">
-                                <input type="number" class="form-control text-uppercase" id="no_cm" name="no_cm">
+                                <input type="number" class="form-control required text-uppercase" id="no_cm" name="no_cm">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Diagnosa</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control text-uppercase" id="diagnosa" name="diagnosa">
+                                <input type="text" class="form-control required text-uppercase" id="diagnosa" name="diagnosa">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Tindakan</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control text-uppercase" id="tindakan" name="tindakan">
+                                <input type="text" class="form-control required text-uppercase" id="tindakan" name="tindakan">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Operator</label>
                             <div class="col-sm-8">
-                                {{-- <input type="text" class="form-control text-uppercase" id="operator" name="operator"> --}}
-                                <select name="operator" class="form-control text-uppercase">
+                                {{-- <input type="text" class="form-control required text-uppercase" id="operator" name="operator"> --}}
+                                <select name="operator" class="form-control required">
                                     @foreach ($operators as $operator)
                                         <option value="{{ $operator }}">{{ $operator }}</option>
                                     @endforeach
@@ -66,7 +66,7 @@
                         <div class="row mb-3">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Ruang Operasi</label>
                             <div class="col-sm-8">
-                                <select name="ruang_operasi" class="form-control text-uppercase">
+                                <select name="ruang_operasi" class="form-control required text-uppercase">
                                     @foreach ($optionKamar as $ruang_operasi)
                                         <option value="{{ $ruang_operasi }}">{{ $ruang_operasi }}</option>
                                     @endforeach
@@ -76,19 +76,19 @@
                         <div class="row mb-3">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Jaminan</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control text-uppercase" id="jaminan" name="jaminan">
+                                <input type="text" class="form-control required text-uppercase" id="jaminan" name="jaminan">
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <label for="colFormLabel" class="col-sm-3 col-form-label">Profilaksis</label>
+                            <label for="colFormLabel" class="col-sm-3 col-form-label">Keterangan</label>
                             <div class="col-sm-8">
-                                <input type="text" class="form-control text-uppercase" id="profilaksis" name="profilaksis">
+                                <input type="text" class="form-control required text-uppercase" id="profilaksis" name="profilaksis">
                             </div>
                         </div>
                         <div class="row mb-4">
                             <label for="colFormLabel" class="col-sm-3 col-form-label">Status</label>
                             <div class="col-sm-8">
-                                <select name="status" class="form-control text-uppercase">
+                                <select name="status" class="form-control required text-uppercase">
                                     @foreach ($statuses as $status)
                                         <option value="{{ $status }}">{{ $status }}</option>
                                     @endforeach
