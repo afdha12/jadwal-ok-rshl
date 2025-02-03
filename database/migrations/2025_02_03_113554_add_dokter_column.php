641d3eb91675e8ffec27b08bf9cf2f4b370830e5
Azri Afdhalluddin <azri.afdha@icloud.com>
2025-02-03 11:49:19 +0700

    minor changes

--- database/migrations/2025_02_03_113554_add_dokter_column.php
+++ database/migrations/2025_02_03_113554_add_dokter_column.php
@@ -24,7 +24,7 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::create("dokter", function (Blueprint $table) {
+        Schema::table("dokter", function (Blueprint $table) {
             $table->dropColumn('spesialis');
         });
     }
