30199153d416696342cdb5fe98341e2948325054
Azri Afdhalluddin <azri.afdha@icloud.com>
2025-02-03 11:48:19 +0700

    fix some error

--- database/migrations/2025_02_03_113554_add_dokter_column.php
+++ database/migrations/2025_02_03_113554_add_dokter_column.php
@@ -12,7 +12,7 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create("dokter", function (Blueprint $table) {
+        Schema::table("dokter", function (Blueprint $table) {
             $table->string('spesialis')->nullable();
         });
     }
