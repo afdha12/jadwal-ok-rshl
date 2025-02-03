97f54ced359b8f2246e701a702e8dbd581af3436
Azri Afdhalluddin <azri.afdha@icloud.com>
2025-02-03 13:29:50 +0700

    Add favicon to display page

--- resources/views/pages/display.blade.php
+++ resources/views/pages/display.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta http-equiv="refresh" content="360">
     <title>Display Jadwal Operasi</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('img/hermina.png') }}">
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
@@ -16,6 +17,7 @@
 
     {{-- Flatpickr --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
